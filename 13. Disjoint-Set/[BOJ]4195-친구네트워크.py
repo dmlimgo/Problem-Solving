8ea27357572e569e6dc9f988752982eeb3081dfb
dmlimgo <dongmyeong.lim@gmail.com>
2019-11-19 23:57:35 +0900

    BOJ 4195 disjoint-set 연습

---
+++ 13. Disjoint-Set/[BOJ]4195-친구네트워크.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find(u):
+    if u == parent[u]:
+        return u
+    parent[u] = find(parent[u])
+    return parent[u]
+
+def merge(u, v):
+    u = find(u)
+    v = find(v)
+    if u != v:
+        parent[u] += parent[v]
+        parent[v] = u
+
+T = int(input())
+for tc in range(T):
+    F = int(input())
+    parent = [-1 for i in range(F+1)]
+    friend = {}
+    index = 0
+    for i in range(T):
+        a, b = input().split()
+        if a not in friend:
+            friend[a] = index
+            index += 1
+        if b not in friend:
+            friend[b] = index
+            index += 1
+        merge(friend[a], friend[b])
+        print(parent[friend[a]])
