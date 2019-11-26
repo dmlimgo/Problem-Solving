1ce59f2dbcb3ea47c755ae7c01de69b23b7f66e5
dmlimgo <dongmyeong.lim@gmail.com>
2019-11-26 18:00:24 +0900

    disjoint set 문제푸는중

--- 13. Disjoint-Set/[BOJ]10775-공항.py
+++ 13. Disjoint-Set/[BOJ]10775-공항.py
@@ -3,42 +3,46 @@ input = sys.stdin.readline
 
 answer = 0
 
+# def find(g):
+#     if g == gates[g]:
+#         if g:
+#             gates[g] = g-1
+#             return 0
+#         else:
+#             return -1
+#     p = find(gates[g])
+#     if p:
+#         gates[g] = p
+#         return p
+#     else:
+#         return -1
 def find(g):
     if g == gates[g]:
-        if g:
-            gates[g] = g-1
-            return 0
-        else:
-            return -1
-    p = find(gates[g])
-    if p:
-        gates[g] = p
-        return p
-    else:
-        return -1
+        return g
+    gates[g] = find(gates[g])
+    return gates[g]
 
-def dock(gi):
+def docking(g):
     global answer
-    g = gi
-    while g >= 0:
-        p = find(g)
-        if p:
-            g = p-1
-        else:
-            answer += 1
-            return 1
-    return 0
+    if g == gates[g]:
+        gates[g] = g-1
+        answer += 1
+        return 1
+    else:
+        root = find(g)
+        if root:
+            gates[g] = root - 1
+        return 0
 
 
 G = int(input())
 P = int(input())
 
 gates = [i for i in range(G+1)]
-print(gates)
 for i in range(P):
-    gi = int(input())
-    docked = dock(gi)
-    print(gates)
+    g = int(input())
+    docked = docking(g)
+    print('gates', i, gates)
     if not docked:
         break
 print(answer)
