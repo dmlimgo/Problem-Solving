50dfbc6c23c2d258d718b2a1696ede8fce319744
dmlimgo <dongmyeong.lim@gmail.com>
2019-11-26 23:30:26 +0900

    BOJ disjoint-set 공항

--- 13. Disjoint-Set/[BOJ]10775-공항.py
+++ 13. Disjoint-Set/[BOJ]10775-공항.py
@@ -1,21 +1,9 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 answer = 0
 
-# def find(g):
-#     if g == gates[g]:
-#         if g:
-#             gates[g] = g-1
-#             return 0
-#         else:
-#             return -1
-#     p = find(gates[g])
-#     if p:
-#         gates[g] = p
-#         return p
-#     else:
-#         return -1
 def find(g):
     if g == gates[g]:
         return g
@@ -31,8 +19,11 @@ def docking(g):
     else:
         root = find(g)
         if root:
-            gates[g] = root - 1
-        return 0
+            gates[root] = root - 1
+            answer += 1
+            return 1
+        else:
+            return 0
 
 
 G = int(input())
@@ -42,7 +33,6 @@ gates = [i for i in range(G+1)]
 for i in range(P):
     g = int(input())
     docked = docking(g)
-    print('gates', i, gates)
     if not docked:
         break
 print(answer)
