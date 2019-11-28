f6ad52f161bf9429d869b08a20680c35cfd08353
dmlimgo <dongmyeong.lim@gmail.com>
2019-11-29 02:03:51 +0900

    9938 solving...

--- 13. Disjoint-Set/[BOJ]9938-방청소.py
+++ 13. Disjoint-Set/[BOJ]9938-방청소.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-def find(a, b):
+def mergea(a, b):
     while True:
         if a == parent[a]:
             parent[a] = b
@@ -17,10 +17,33 @@ def find(a, b):
             a = p
             b = -1
 
+def merge(a, b):
+    while True:
+        if a == parent[a]:
+            parent[a] = b
+            break
+        p = parent[a]
+        parent[a] = b    
+        a = p
+        b = -1
+
+def find(n):
+    while True:
+        if n == parent[n]:
+            return 1
+        elif parent[n] == -1:
+            return 0
+        else:
+            n = parent[n]
+
+
 N, L = map(int, input().split())
 parent = [i for i in range(L+1)]
 for i in range(N):
     a, b = map(int, input().split())
-    find(a, b)
+    if find(a):
+        merge(a, b)
+    elif find(b):
+        
     print([i for i in range(0, 11)])
     print(parent)
