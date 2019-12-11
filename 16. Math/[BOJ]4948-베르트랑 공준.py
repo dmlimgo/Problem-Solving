619ded879c5d278348fd148a38db354acb76e7b3
dmlimgo <dongmyeong.lim@gmail.com>
2019-12-12 00:20:28 +0900

    BOJ 에라토스테네스 improved

--- 16. Math/[BOJ]4948-베르트랑 공준.py
+++ 16. Math/[BOJ]4948-베르트랑 공준.py
@@ -2,15 +2,18 @@ import sys
 input = sys.stdin.readline
 
 nums = [1 for _ in range(246913)]
+nums[0] = nums[1] = 0
+cnt = 0
 for i in range(2, 246913):
     if nums[i]:
+        cnt += 1
+        nums[i] = cnt
         for j in range(2, 246912//i+1):
             nums[i*j] = 0
+    else:
+        nums[i] = cnt
+
 n = int(input())
 while n:
-    cnt = 0
-    for i in range(n+1, 2*n+1):
-        if nums[i]:
-            cnt += 1
-    print(cnt)
+    print(nums[2*n]-nums[n])
     n = int(input())
