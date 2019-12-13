24b62746f0939a0da0e76bb854e01cc6efa6e5f8
dmlimgo <dongmyeong.lim@gmail.com>
2019-12-14 01:39:36 +0900

    BOJ math solve

--- 16. Math/[BOJ]골드바흐의 추측.py
+++ 16. Math/[BOJ]골드바흐의 추측.py
@@ -1,23 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-sosu = []
 nums = [1 for i in range(10001)]
+parent = [0 for i in range(10001)]
 nums[0] = nums[1] = 0
+before = -1
 for i in range(2, 10001):
     if nums[i]:
-        sosu.append(i)
+        parent[i] = before
+        before = i
         for j in range(2, 10000//i+1):
             nums[i*j] = 0
-# print(sosu)
-# print(len(sosu))
+    else:
+        parent[i] = before
+
 T = int(input())
 for tc in range(T):
     n = int(input())
-    L, R = 0, 1228
-    switch = 0
-    for i in range(1229):
-        for j in range(1229):
-            if sosu[i] + sosu[j] == n and (abs(sosu[i] - sosu[j])<abs(L-R)):
-                L, R = sosu[i], sosu[j]
-    print(L, R)
+    l = n//2
+    while True:
+        if nums[l] and nums[n-l]:
+            print(l, n-l)
+            break
+        else:
+            l = parent[l]
