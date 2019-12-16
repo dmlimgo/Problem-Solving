4b820035b2c1859e85ca6652ed3726aea8669518
dmlimgo <dongmyeong.lim@gmail.com>
2019-12-16 23:30:59 +0900

    BOJ 1차원배열, 함수 다풀기

--- 90. 언어 연습/[BOJ]단계별-1차원배열.py
+++ 90. 언어 연습/[BOJ]단계별-1차원배열.py
@@ -1,10 +1,19 @@
+#3052 나머지
+arr = []
+for i in range(10):
+    n = int(input())%42
+    if n not in arr:
+        arr.append(n)
+print(len(arr))
+
+
 #2562 최댓값
-maxVal= 0
-maxIndex = 0
-for i in range(9):
-    n = int(input())
-    if maxVal < n:
-        maxVal = n
-        maxIndex = i+1
-print(maxVal)
-print(maxIndex)
+# maxVal= 0
+# maxIndex = 0
+# for i in range(9):
+#     n = int(input())
+#     if maxVal < n:
+#         maxVal = n
+#         maxIndex = i+1
+# print(maxVal)
+# print(maxIndex)
